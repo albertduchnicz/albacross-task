5575a9bac32f23bdaf8c50839472f5eb016de63d
Albert Duchnicz <albert.duchnicz@gmail.com>
2021-11-01 22:51:09 +0100

    updated link to Binance

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import myCss from './footer.module.css';
 
-export const Footer = (): JSX.Element => <div className={myCss.footer}><a href="https://github.com/binance-exchange/binance-official-api-docs">Connected to BINANCE API</a></div>;
+export const Footer = (): JSX.Element => <div className={myCss.footer}><a href="https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md">Connected to BINANCE API</a></div>;
